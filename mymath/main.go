70bf29b5b26017068066f2dd8effd2769d1bc414
Eugene Shamshin <jenkatop1intheworld@gmail.com>
2024-09-10 20:02:31 +0500

    1

--- mymath/main.go
+++ mymath/main.go
@@ -5,12 +5,27 @@ import "math"
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
+
 func Acos(x float64) float64 {
 	return math.Acos(x)
 }
+
 func Acosh(x float64) float64 {
 	return math.Acosh(x)
 }
+
 func Asin(x float64) float64 {
 	return math.Asin(x)
+}
+
+func Max(a, b float64) float64 {
+	return math.Max(a,b)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Yn(y, n float64) float64 {
+	return math.Yn(y, n)
 }
